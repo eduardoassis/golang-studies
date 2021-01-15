e5f64d7b804385a2aea10a20c69dafe570f99cf4
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2021-01-15 14:48:58 +0000

    Calling third party function

--- functions/function.go
+++ functions/function.go
@@ -1,12 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gopkg.in/kyokomi/emoji.v1"
+)
 
 func main() {
 	fmt.Println("Calling the function: main")
 	anotherFunction()
+
+	thirdPartyFunction()
 }
 
 func anotherFunction() {
 	fmt.Println("Calling the function: anotherFunction")
+}
+
+func thirdPartyFunction() {
+	fmt.Println("Hello World Emoji. :tada:")
+	emoji.Println("Do you like :coffee:? Of course you like!")
 }
