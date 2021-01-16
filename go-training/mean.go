3252b011a8e9202a925f3bab99e52ddde8db71e7
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2021-01-16 17:49:17 +0000

    Using the operator ':=' to infer the variable type

--- go-training/mean.go
+++ go-training/mean.go
@@ -7,13 +7,12 @@ import (
 
 func main() {
 
-	var x float64 = 1
-	var y float64 = 2
+	x := 1.0 // the operator ":=" infers the type of the variable, in this case a float
+	y := 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean float64
-	mean = (x + y) / 2
+	mean := (x + y) / 2.0
 	fmt.Printf("result:%v, type of %T\n", mean, mean)
 }
