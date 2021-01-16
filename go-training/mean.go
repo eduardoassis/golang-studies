18ec33b117f5e3262d0aae59c9488cf8eb70d5c8
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2021-01-16 17:52:08 +0000

    Refactoring

--- go-training/mean.go
+++ go-training/mean.go
@@ -7,8 +7,7 @@ import (
 
 func main() {
 
-	x := 1.0 // the operator ":=" infers the type of the variable, in this case a float
-	y := 2.0
+	x, y := 1.0, 2.0 // the operator ":=" infers the type of the variable, in this case a float
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
